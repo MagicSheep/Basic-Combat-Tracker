e13da133c1f3435b8cc0aa959c12fdc6145af79c
Brandon Black <brandonmblack98@gmail.com>
2021-01-22 03:09:11 -0500

    Update CombatTracker.java
    Move some UI elements around. Left room for future elements to be added.

--- Not so Advanced Combat Tracker/src/com/magicsheep/combattracker/CombatTracker.java
+++ Not so Advanced Combat Tracker/src/com/magicsheep/combattracker/CombatTracker.java
@@ -24,8 +24,8 @@ import java.awt.Color;
  * JCheckbox MageArmor: This checkbox takes whatever number ACSpinner has and adds 4 if checked and removes 4, restoring the original value when unchecked.
  * JCheckbox NebbyBox: Keeps track of the status of my summon. Similarly to the HP/MP Spinners, does nothing other than being a visual queue.
  * JLabel ActiveArts: Updates with the context of the button pushed.
- * JButton DarkArtsButton: Udpates the label ActiveArts to say "Dark Arts active."
- * JButton LightArtsButton: Udpates the label ActiveArts to say "Light Arts active."
+ * JButton DarkArtsButton: Updates the label ActiveArts to say "Dark Arts active."
+ * JButton LightArtsButton: Updates the label ActiveArts to say "Light Arts active."
  * Int AC: (Used in MageAromr's action listener) is the value of whatever is in ACSpinner.
  * Int ACPlus: (Used in MageArmor's action listener) is uses to add or subtract 4 from AC.
  * JLabel ArcaneLabel: Holds the text "Arcane Res:"
@@ -56,7 +56,7 @@ public class CombatTracker {
 		JLabel ArcaneLabel = new JLabel("Arcane Res:");
 		JSpinner ArcaneSpinner = new JSpinner();
 		JCheckBox MageArmor = new JCheckBox("Mage Armor");
-		JCheckBox NebbyBox = new JCheckBox("Is Nebby active?");
+		JCheckBox NebbyBox = new JCheckBox("Is Nebby summoned?");
 		ActiveArts = new JLabel("No Arts active.");
 		DarkArtsButton = new JButton("Dark Arts");
 		LightArtsButton = new JButton("Light Arts");
@@ -116,20 +116,23 @@ public class CombatTracker {
 
 		
 		//Set the size of stuff (x, y, width, height)
-		CurrentHP.setBounds(110, 15, 20, 20);
-		CurrentMP.setBounds(110, 40, 25, 20);
-		HPSpinner.setBounds(135, 15, 50, 20);
-		NebbyHP.setBounds(190, 15, 50, 20);
-		MPSpinner.setBounds(135, 40, 50, 20);
-		ACLabel.setBounds(110, 65, 25, 20);
-		ACSpinner.setBounds(135, 65, 50, 20);
-		MageArmor.setBounds(110, 90, 100, 25);
-		NebbyBox.setBounds(110, 115, 150, 25);
-		ActiveArts.setBounds(115, 145, 120, 25);
-		DarkArtsButton.setBounds(100, 165, 120, 25);
-		LightArtsButton.setBounds(100, 195, 120, 25);
-		ArcaneLabel.setBounds(125, 225, 150, 20);
-		ArcaneSpinner.setBounds(135, 250, 50, 20);
+		CurrentHP.setBounds(20, 35, 20, 20);
+		CurrentMP.setBounds(20, 60, 25, 20);
+		HPSpinner.setBounds(100, 35, 50, 20);
+		NebbyHP.setBounds(155, 35, 50, 20);
+		MPSpinner.setBounds(100, 60, 50, 20);
+		ACLabel.setBounds(20, 85, 25, 20);
+		ACSpinner.setBounds(100, 85, 50, 20);
+		ArcaneLabel.setBounds(20, 110, 150, 20);
+		ArcaneSpinner.setBounds(100, 110, 50, 20);
+		
+		NebbyBox.setBounds(160, 60, 150, 25);
+		MageArmor.setBounds(160, 85, 100, 25);
+		
+		ActiveArts.setBounds(35, 215, 120, 25);
+		DarkArtsButton.setBounds(20, 235, 120, 25);
+		LightArtsButton.setBounds(20, 265, 120, 25);
+
 		
 		//Adjust the colors to work with the frame's black background
 		CurrentHP.setForeground(Color.WHITE);
@@ -140,8 +143,6 @@ public class CombatTracker {
 		NebbyBox.setForeground(Color.WHITE);
 		NebbyBox.setBackground(Color.BLACK);
 		ActiveArts.setForeground(Color.WHITE);
-		DarkArtsButton.setBackground(Color.LIGHT_GRAY);
-		LightArtsButton.setBackground(Color.LIGHT_GRAY);
 		ArcaneLabel.setForeground(Color.WHITE);
 		
 		//Add the things to the frame
@@ -161,7 +162,7 @@ public class CombatTracker {
 		frame.add(ArcaneSpinner);
 		
 		//Fancy frame stuff
-		frame.setSize(335, 325); //width, height
+		frame.setSize(335, 335); //width, height
 		frame.setLayout(null); 
 		frame.getContentPane().setBackground(Color.BLACK);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
